5cde1b2dddfc25c471999edeb127ede150d7251a
hongsheng <24836227@qq.com>
2023-01-07 22:39:05 +0800

    ADD 入口模块

---
+++ lib/jiaowei.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   jiaowei.py
+@Time    :   2023/01/07 18:01:52
+@Author  :   Hongsheng He
+@Version :   1.0
+@Contact :   24836227@qq.com
+@License :   (C)Copyright 2007-2020, hongsheng
+@Desc    :   None
+'''
+
+import os
+import sys
+
+from lib.logger import logging
+from lib.dict import JWDict
+
+
+class JiaoWei:
+    work_dir = ""
+    zero_file = ""
+    jwDict = None
+
+    def __init__(self, work_dir: str, zero_file: str) -> None:
+        
+        logging.info("work_dir:%s" % work_dir)
+        logging.info("zero_file:%s" % zero_file)
+        
+        self.work_dir = work_dir
+        self.zero_file = os.path.join(work_dir, "docs", zero_file)
+        
+
+    def LoadZero(self) -> Exception:
+        if not os.path.exists(self.zero_file):
+            return Exception("Error:零号文件不存在,%s" % self.zero_file)
+
+        self.jwDict = JWDict(self.work_dir, self.zero_file)
+        return None
+
+    def Run(self):
+        logging.info("开始生成交维数据")
+
+        err = self.LoadZero()
+        if err:
+            logging.error("加载零号文件失败,%s" % err)
+            sys.exit(1)
+
+        # err = self.LoadDict()
+        # if err:
+        #     logging.error("加载字典失败,%s" % err)
+        #     sys.exit(1)
+
+        logging.info("生成交维数据结束")
