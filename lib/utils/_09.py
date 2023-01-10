0009e736640580ed56961a9230dc67ac17559b6c
hongsheng <24836227@qq.com>
2023-01-10 16:06:10 +0800

    ADD 获取字典值、字典函数

--- lib/utils/_09.py
+++ lib/utils/_09.py
@@ -13,12 +13,53 @@ import re
 
 import pandas as pd
 
+from lib.dict import JWDict
 
-def Copy(zero, target_data_frame, col_name, value, source_sheet):
+
+def Copy(zero, jwDict: JWDict,  target_data_frame, col_name, value, source_sheet, source_column):
     df = target_data_frame
     if not source_sheet or not source_sheet:
         df[col_name] = "待确认: 源sheet或源列未指定"
         return df, False
 
-    df[col_name] = zero.GetData(source_sheet)[value]
+    df[col_name] = zero.GetData(source_sheet)[source_column]
     return df, True
+
+
+def fetchDictValue(fetchedDict: dict, match_value: str, dict_name: str) -> str:
+    """匹配字典内容
+
+    Args:
+        jwDict (JWDict): 字典对象
+        match_value (str): 需要匹配的值，例如系统名称
+        dict_name (str): 字典名称
+
+    Returns:
+        str: 字典的键值
+    """
+
+    for k, v in fetchedDict.items():
+
+        if re.search(k, match_value):
+            return v
+
+    return "待确认: %s字典无法匹配到:%s" % (dict_name, match_value)
+
+
+def GetDict(zero, jwDict,  target_data_frame, col_name, value, source_sheet, source_column):
+    df = target_data_frame
+    if not source_sheet or not source_column or not col_name or not value:
+        df[col_name] = "待确认: GetDict需要指定source_sheet、source_column、value、col_name"
+        return df, False
+
+    # 获取原始值
+    df[col_name] = zero.GetData(source_sheet)[[source_column]]
+
+    # 匹配字典
+    fetchedDict = jwDict.GetDict(value)
+
+    df[col_name] = df.apply(
+        lambda row: fetchDictValue(fetchedDict, row[col_name], value), axis=1)
+    return df, True
+
+
