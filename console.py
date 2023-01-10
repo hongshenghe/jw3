1da63e46f293a8d9ebb5dc3333e62cfe7f244d21
hongsheng <24836227@qq.com>
2023-01-10 16:08:39 +0800

    ADD console文件增加测试获取生成产品线

--- console.py
+++ console.py
@@ -17,19 +17,34 @@ import pandas as pd
 from lib.base import ensurePath, generate_batchid
 from lib.rule import JWRule, LoadRules
 from lib.zero import JWZero
+from lib.dict import JWDict
 
 work_dir = Path(__file__).resolve().parent
 
+# zero 测试
 zero = JWZero(work_dir,
               os.path.join("upload", "00-天翼云集成实施基本信息表模板(网络和服务器设备表含公式)20230101(1).xlsx"))
-# jwRules = LoadRules(work_dir, zero)
+# # jwRules = LoadRules(work_dir, zero)
 
-batch_id = generate_batchid()
-rule = JWRule(batch_id, work_dir, "05", zero)
+# batch_id = generate_batchid()
+# rule = JWRule(batch_id, work_dir, "05", zero)
 
-err = rule.Generate()
-if err:
-    print("error:", err)
+# err = rule.Generate()
+# if err:
+#     print("error:", err)
 
+
+# dict 测试
+jwDict = JWDict(work_dir,
+                os.path.join("upload", "dict.xlsx"))
+
+
+
+# 测试获取生成产品线
+network = zero.GetData("网络设备")[['对应设备清单-配对列', '机架', '机房']]
+server = zero.GetData("服务器")[['对应设备清单-配对列', '机架', '机房']]
+dfSummary = pd.concat([network, server], axis=0).sort_values(
+    by=['对应设备清单-配对列', '机架', '机房'], ascending=True)
+asserts = zero.GetData("设备清单")
 
  
