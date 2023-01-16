66511adbb5a6e88244388940c119fa4e92782129
hongsheng <24836227@qq.com>
2023-01-16 11:17:47 +0800

    add logic code

--- lib/utils/_09.py
+++ lib/utils/_09.py
@@ -15,7 +15,7 @@ import pandas as pd
 
 from lib.dict import JWDict
 from lib.zero import JWZero
-from lib.utils.base import _fetchDictValue, _getAsssetInfo
+from lib.utils.base import _fetchDictValue, _getAsssetInfo, _getNetworkLogicCode
 
 
 def Copy(zero: JWZero, jwDict: JWDict, target_data_frame, col_name: str, value: str, source_sheet: str, source_column: str):
@@ -71,5 +71,21 @@ def GetSubProductLine(zero: JWZero, jwDict: JWDict, target_data_frame, col_name:
     # 生成细分产品线
     df[col_name] = df.apply(
         lambda row: _fetchDictValue(sub_product_dict, row[col_name], "细分产品线"), axis=1)
+
+    return df, True
+
+
+def GetNetworkLogicCode(zero: JWZero, jwDict: JWDict, target_data_frame, col_name: str, value: str, source_sheet: str, source_column: str):
+
+    # 逻辑编码
+    df = target_data_frame
+
+    # 生成原始数据
+    # source_data = zero.GetData(source_sheet)
+
+    # 生成逻辑编码
+    # df[col_name] = source_data.apply(
+    #     lambda row: _getNetworkLogicCode(row['堆叠后名称/M-LAG（逻辑名称）'], row['设备标签']), axis=1)
+    df[col_name] = '-'
 
     return df, True
